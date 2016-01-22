0c5629bc80def6e225ae826904dcaf5c49a5f9b7
Manuel Rueda <manuel.rueda.un@gmail.com>
2016-01-22 20:34:31 -0300

    Better validation and add zip support

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # repo-copy
 Git repository copy/compressor
+## Usage
+
+```
+Usage: repo-copy [options] <repo path ...>
+
+Options:
+
+  -h, --help        output usage information
+  -V, --version     output the version number
+  -c, --copy        Copy repository
+  -z, --compress    Compress repository
+  -o, --out [stat]  Output (repo.tar.gz or copyFolder/)
+
+```
--- src/repo-copy.js
+++ src/repo-copy.js
@@ -8,9 +8,30 @@ var glob = require('glob');
 var fstream = require('fstream');
 var tar = require('tar');
 var zlib = require('zlib');
+var AdmZip = require('adm-zip');
 var mkdirPromise = Promise.denodeify(fs.mkdir);
 
 module.exports = {
+  validateProgram: (program) => {
+    if (program.copy){
+      if (program.args.length === 0)
+        return new Error('Need to provide a repository path');
+      try {
+        var stat = fs.statAsync(program.out);
+        if (!stat.isDirectory()){
+          return new Error('The output must be a folder');
+        }
+      } catch (e) {
+        return new Error('The output folder can\'t be founded');
+      }
+    }else if (program.compress){
+      if (program.args.length === 0)
+        return new Error('Need to provide a repository path');
+    }else{
+      return false;
+    }
+    return true;
+  },
   isGitRepo: (path) => {
     return new Promise((resolve, reject) => {
       fs.stat(path, (err, stat) => {
@@ -41,7 +62,9 @@ module.exports = {
   createCopy: (base, files, tempPath) => {
     var proms = [];
     //Add the promise to create the temp folder
-    proms.push(mkdirPromise(tempPath));
+    proms.push(() => {
+      return mkdirPromise(tempPath);
+    });
 
     //add all the promises that copy the files
     files.forEach((file) => {
@@ -49,50 +72,65 @@ module.exports = {
     });
 
     //add the promise to return the temp folder
-    proms.push(Promise.resolve(tempPath));
+    proms.push(() => {
+      return Promise.resolve(tempPath);
+    });
 
     //return the result of all promises
-    return Promise.all(proms)
+    return proms.reduce((cur, next) => {
+      return cur.then(() =>{
+        return next();
+      });
+    }, Promise.resolve());
   },
-  createTarGz: (tempPath, destination) => {
+  createTarGzip: (tempPath, destination) => {
 
     fstream.Reader({ path: tempPath, type: 'Directory' }) /* Read the source directory */
     .pipe(tar.Pack()) /* Convert the directory to a .tar file */
     .pipe(zlib.Gzip()) /* Compress the .tar file */
     .pipe(fstream.Writer({ path: destination })); /* Give the output file name */
 
+  },
+  createZip: (tempPath, destination) => {
+    var zip = new AdmZip();
+
+  	zip.addLocalFolder(tempPath);
+  	// or write everything to disk
+  	zip.writeZip(destination);
   }
 };
 
 function copyFile(source, target){
-  return new Promise((resolve, reject) => {
-    fs.stat(source, (err, stat) => {
-      if (err){
-        reject(err);
-      }else{
-        if (stat.isDirectory()){
-          fs.mkdir(target, (err) => {
-            if (err){
+  return () => {
+    return new Promise((resolve, reject) => {
+      fs.stat(source, (err, stat) => {
+        if (err){
+          reject(err);
+        }else{
+          if (stat.isDirectory()){
+            fs.mkdir(target, (err) => {
+              if (err){
+                reject(err);
+              }else{
+                resolve();
+              }
+            })
+          }else {
+            var rd = fs.createReadStream(source);
+            rd.on("error", function(err) {
               reject(err);
-            }else{
+            });
+            var wr = fs.createWriteStream(target);
+            wr.on("error", function(err) {
+              reject(err);
+            });
+            wr.on("close", function(ex) {
               resolve();
-            }
-          })
-        }else {
-          var rd = fs.createReadStream(source);
-          rd.on("error", function(err) {
-            reject(err);
-          });
-          var wr = fs.createWriteStream(target);
-          wr.on("error", function(err) {
-            reject(err);
-          });
-          wr.on("close", function(ex) {
-            resolve();
-          });
-          rd.pipe(wr);
+            });
+            rd.pipe(wr);
+          }
         }
-      }
+      });
     });
-  });
+  };
 }
